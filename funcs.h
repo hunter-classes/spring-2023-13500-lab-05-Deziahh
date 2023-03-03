c0769a00ecc1f824d8fcb57f49dc2b85e0813ff8
Deziah Gayle <81055368+Deziahh@users.noreply.github.com>
2023-03-03 03:55:45 -0500

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -3,3 +3,15 @@
 // add prototypes here
 
 bool isDivisibleBy(int n, int d);
+
+bool isPrime(int n);
+
+int nextPrime(int n);
+
+int countPrimes(int a, int b);
+
+bool isTwinPrime(int n);
+
+int nextTwinPrime(int n);
+
+int largestTwinPrime(int a, int b);
